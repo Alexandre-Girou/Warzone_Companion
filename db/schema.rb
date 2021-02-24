b658b4a439592a3834b7e580d3a1855db05b63fd
Alexandre Girou <agirou5@gmail.com>
2021-02-24 15:42:05 +0100

    Half finished PGSearch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_151262) do
+ActiveRecord::Schema.define(version: 2021_02_24_132120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,15 @@ ActiveRecord::Schema.define(version: 2021_02_22_151262) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["card_id"], name: "index_favorites_on_card_id"
     t.index ["deck_id"], name: "index_favorites_on_deck_id"
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
   end
 
   create_table "users", force: :cascade do |t|
