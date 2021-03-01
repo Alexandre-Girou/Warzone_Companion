ebe23e8a64371b3fdc55de0ee7a3a2910ac45019
Alexandre Girou <agirou5@gmail.com>
2021-03-01 16:48:21 +0100

    Add Comments Show and create on card#show

--- app/models/card.rb
+++ app/models/card.rb
@@ -7,6 +7,7 @@ class Card < ApplicationRecord
 
   has_many :favorites, dependent: :destroy
   has_many :decks, through: :favorites
+  has_many :comments
 
   scope :category, ->(category) { Card.where(category: category) }
   scope :level, ->(level) { Card.where(level: level) }
