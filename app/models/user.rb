d8fa33791b9a5e32d9877686ec7bce1f4fa85e52
Alexandre Girou <agirou5@gmail.com>
2021-03-01 12:11:09 +0100

    Adding Cloudinary, make it working for Users Registration and new cards

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :decks, dependent: :destroy
   has_many :favorites, through: :decks
   has_many :cards
+  has_one_attached :photo
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :username, presence: true
 end
